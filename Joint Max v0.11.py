7cd9211fb9b2968e709e79adb1f4a2604869c00d
Vladimir <46215231+vladliash@users.noreply.github.com>
2022-08-05 15:32:26 +0300

    Add files via upload

---
+++ Joint Max v0.11.py
@@ -0,0 +1,63 @@
+# PNG Base64:iVBORw0KGgoAAAANSUhEUgAAAgAAAAIACAMAAADDpiTIAAAApVBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABU/knhAAAANnRSTlMA+ksCUQWv9KFlhJ0/C+0wA8B0exMcbZrl8VjqF2CputTdIQ+1yuKAz6M3SC2QQ8NpJ5aIU3FYAVJOAAAQP0lEQVR42uzZy1JaURCF4W2CEi+IstXgDSElaqFVkkCt93+0TDJIUpvTR4er/2/cs/XPuuBj7gbPs+V0KA2ny9nz4LIgkfHRXP+Zb1YFKYzWczXNr0YF7kaTW+10OyEBc4OFOj1tC3xdzBQ6uCgwtXpSD9OTAks3e+pl77DA0ES91QI7VR9wVGCmShSQWJUoILEqUUBiVaKAxKpEAYlViQISqxIFJFYlCkisShSQWJUoILEqUUBiwf7L+vr29lrfKcBU9/7vq/LH+JwCHHXv//z171MK8NM96lX5xyEFuAn3pwBr4f4UYC3cnwKshftTgLVwfwqwFu5PAdbC/SnAWrg/BVgL96cAa+H+FGAt3J8CrIX7U4C1cH8KsBbuTwHWwv0pwFq4PwVYC/enAGvh/hRgLdyfAqyF+1OAtXB/CrAW7k8B1sL9KcBauD8FWAv3pwBr4f4UYC3cnwKsTaL9KcDaTbQ/BVgbD4P9KcDaaBHsTwHejoP9KcDbSbA/BXg7mwf7U4C3+3B/CnB2toj3pwBjgx77U4Cxx177U4Cru71++1OAqXXf/SnA02Pv/SnA0f6w9/4U4Gispk1poQA/92p52S9NFGBno5YvZQcKcDNTw1PZiQLMnKvhV9mNAry8qGFdOlCAlbkatqULBThpBnBTOlGAkaUaJqUbBfh4UMOsBCjAxrEarkclQAEu6ic/ARRgYquW07sSoQAPP9R0vl8iFOBhqqYDCkjimyggtYEoILXRNQXk9l0UkNpYFJDbAwXkthIF5DajgNwuhxSQ21oUkNsjBeR2saCA3H6eUkBuqyEF5HZCAclRQHYUkB0FZEcB2VFAdhSQHQVkRwHZUUB2FJAdBWRHAdlRQHYUkB0FZEcB2VFAdhSQHQVkRwHZUUB2FPCbvTMmbgAIgthkki4AzMT8odlHwHKtlShI5d/+OhWwTgWM8/fzrIBhzn8F7HL+K2CX818Bu5z/Ctjl/FfALue/AnY5/xWwy/mvgF3OfwXscv4rYJfzXwG7nP8K2OX8V8Au578Cdvns/9H7ADng/7cXIm7If2+E3JD/Xom5If+9E3RD/nsp6ob891bYDfnvtbgb8t+9gBvy38WIG/LfzZAb8t/VmBvy392gG/Lf5agb8t/tsBvy3/W4G/LffoAb8t+ChBvy34aIG/Lfiowb8t+OkBvy35KUG/Lflpgb8t+anBvy356gG/LfoqQb8t+mqBvy36qsG/LfrrAb8t+ytBvy37a4G/Lfurwb8t//Am7Ifz9MuGH/FWDms///t/8KMMP+K8AM+68AM+y/Asyw/woww/4rwAz7rwAz3/ifLuDFzp2tNBAFQRhugysu0UQl7kjMohERlH7/R5PAXLi008nNSVXNqVf4ftps49Ckt5p/pws4MOGt6t/lAraOTHar+3e5gNG5iW4d/y4XsGuaW8+/ywV8muLW9e9wAdNj09v6/h0uYGBy27g/VQE3cicAwJ+qgHfTGoQ/UwFzkxqIP1MBfRMajD9RAUpfCgH58xQg9DcAyp+ngFsTGZg/XgFDD7dtGoPzxyvgzKPNTGKA/nAF9D3avSkM0h+ugIkHm5jAQP3RChh6sKnxD9YfrICFB7sy+gH7YxXQ92Bbxj5of6gCNAMA90cqoKf4JwDeH6iAmeCLQAJ/nALmem8DKfxRCnjR+yCIxB+jgNOJ3CNiNP4QBXx4uJ7RjsgfoICBx+N9PIjKf+MFDOR+EELmX6CAzF/r22A6/00UkPv7q3GO0L9gAbG/0qcAlP7FC8j9/c4oR+pfuoDcf8T5aBitf9kCcn+/NMYR+xctIPd/3DHCtfv7w4VBLyugoL8vjHCJv/sV+AubrIBy/s9GuMaf+QhkBZTyfxwb3xp/7iOQFVDGf4/xweDGP98T9BHICijh79fGt8af/wiUKGDpL/YOsPFXOAIFChi4i/17qMZf4whkBVT///xVjsCbtxdQ/WN/lSNwuu9JAdU/8lc5Akv/rIDq/9df5Qg0/kkB1f+3v8oRaPzTAqr/T3+VIxD5xwVU/+/+Kkcg9o8LqP6p/+Ghr7ATnCMQ+LcUUP3z338spkxHIPBvLaD6Z/5mY6IjEPgnBVT/xH+53ojkCAT+aQHVP/PnOQKRf15A9W/35zkCsX9eQPVv8+c5Ai3+SQHV/4u9u11KIwiiMDwIgoqoRPwACaKRiASjmLj3f2lhSqoCyUrP9u603eM5F2BZ9bTvr0He97cTAe/PvQD4v+dvJwLb/WvE+wD45/vbiQDhv0O9EIF/nr+dCFD+9Bsh+P/vbycCtD99AfCn/Ffr6YsA7R9yAfDP8+dHoDl0xGT96QuA/7q/nQjQ/qEXAP+//nYiQPuHXwD8vX/QentKIkD7F7kA+L/524kA7V/sAuDvZycCtH/RC4C/c3YiQPsXvYDu2i+7+wj/aiIwcKsp8Kcu4Oi145xfvTGGv+4I0P6cC8hazS+DwXn7MoO/7gjQ/lsugB78lUeA7+8vAP75/nYiQPtvvwD4b6z75m8nArQ/dQHw31i755yzEwHan74A+G/scsfZiQDtH3IB8N9c+1u5CDyERUCJv78A+NuMAO0fegHwtxgB2j/8AuBvLwK0f5ELgL+1CND+xS4A/lVHoBEYAQ3+/gLgbykCDH9is6NsbZ/8//+qjwDpz9jiJgtaq+HsbeWfSgRIf9Z6d1nAxha/A9L7M/f9h74IkP7cPfczai8WvwFu5Z9KBEh//s5G2dZNZ87gVv6pRCCev9/sekv9B7q/K4/lX1MTgauBAv/l5i/93J/c3DHJT/i3z+5EInAVFgEF/st1Gnv/RKvf3B86m6Pf/3zth+Bc6IgAw5+3s8bpxd1kOp1cPxwM5jb/9kPffxmKAOmPFfd3diIA/xj+2iIwGsJf0t9OBOAfw99OBE4e4B/BXzwCo7AIwF/a30fgWm8E4B/D304E4B/Dnx+BnzIRgL+wv9++wgjAX8BfOAK/wyLQg7+APzMCHZEIwD+Ov50IDOEfwd9QBGrwr97fVgTgL+DPjcBL2QjAX6l/aARuSkcA/jr91UcA/jz/VCIAf55/KhGAv4C/361IBAZX8NfprzQC8Of5pxIB+Bf0b82cXyoRgH8hf7/aryVOKhGAf54/tXHJCDxLRGA4gn8JfxUROCwbAfhz/T9HBODP9PerHezqj8DJBP4cf6EItLKAPYZGAJ//qMw/lQjg8188/1QiAH+efyoRgL+Av4/Asc4IwJ/pLx+BblgE4L8xNf46IwB/nn8qEYA/zz+VCMC/Uv9uzVgE4F/x+4/FRCAC9fPACMBf2t+53VNDEYB/jPdfczMRgH8M/+AI3MtEAP6y/nYiAP8Y/nYiAP8Y/nYiAP8Y/nYiAP8Y/qwITD8iAnX4R/C3EwH4x/C3EwH4C/jLRuApLALwj+dvJwLwj+FvJwLwj+FvKALwl/b3EbjXEwH4i/v7zacSEXhtwV+nv1QEFk/w1+lvIALw5/mnEgH4R/b3O9YbAfjz/FOJAPx5/qlEAP48/1QiAH+ef9wIzMtGAP6x/LOnhVsulQjAn/DPWeu17lKJAPwp//wI3CYSAfgz/P1a50lEAP7v+WuJwGnZCMCf5a8nApOiEcD73wr8U4kA/Hn+qUQA/jz/VCIAf55/KhGAv4C/X1dnBODP9ZePwDgsAvBfnx5/lRGAP88/lQjAn+efSgTgX6l/+9FYBOBf9fuPwxuJCBwERgD+0v7OdTRFYAF/WX8/OxGAfwz/8Aj8YedcltoGgijaJoAhjh0eVhn8EjGPAE5MEsj8/6cFFxtSNVZLLfWke3LP2iudq1NajOcHJYkA/Cf1by4C/fMD+E/n308E4F/Dv58IwL+Gfz8RgH8N/7IIlEkicHYC/2n8p4/AY80IwL+ufz8RgH8N/34iAP8a/v1EAP71/McZ/go1uEwUAfjX8O8oAqfw371/VxGAfwX/PJsUERg+9+Dfpn8PEYD/hv4nb/5ziQD8K/gXReC4bQTg36Z/0xGAf5n/XCIA/zL/uUQA/mX+c4kA/Df0//TmXzkCRdsIwL/a/78O50SUSwTgP+afYTalVmzuzEQA/mP+eZaZRAD+d/n/PyIA/4x/zQhMkkSgD/+N2Hvzn0sEcP5D9sIkisB1zQjAfypWs7AllwjAf0MObkNjluMUEVjAfwqeA4NGBNY1IwD/6jz2wiu5RAD+G3MbhMxeUkTgpoB/VaZhSyYRgP/GnFyFVzKJAPw3Zz/spO8tAvAvoAy7GMyXviIA/wJWlef/X2aOIgD/Es6q/BON/UQA/kU8VfqnVBEYhBqMFvDfNcMe4792BPbTRAD+u2UToiw/0Dum9iMA/51+AlwNaYufCMC/lPiDW9OW5hF4SBMB+O+O0xDhPvLDeZII7F3UiwD8d8WXEOGcXvETAfhvwShE+EREfiIA/23ohwgrInITAfhvRS9EKGiLPAIXiSIA/0oF+EZETiIA/y2JvmtTqmB+aCcChwX8t+Q+RDgiIh8R6MF/S6Kvc39M5CUC8K9xIvw7kYEIjAfwr840/vA2RM4jAP/1+BmijD7z0xmliMAR/CtzxyygdQT22kYA/lWZhJ0L8BsB+K/POuxegNcIwH8DDi7YBbSPwFHbCMC/Hh9DxQI8RgD+m1H0qhbgLwLwL/0MjC/AWwTgvzFFv3IBviIA/wK+hsoFeIoA/EsYXlUvwFAEHuBfg8de9QLsRODDE/xr8DtUL8BKBHD+R4sBswAbEYB/NQ7KThZAi1oRGIzh3xrDU2YB/z4C8P8eswtQi8AJ/P+N2QXUjQD8G0O4AHkEruHfFtIFyCMA/7YQL0AeAfg3RYcLKG7qRQD+TcEuQCEC8G8JdgEKEYB/S7AL0IgA/BtCvAB5BO7W8G8IdgEqEYB/O7ALUIkA/NuBXYBOBODfDOwCdCIA/2boeAG0uAw1mFzDvxXYBShFAP6twC5AKwLwbwR2AVoRgH8jsAtQiwD824BdgFoE4N8G7ALUIgD/NmAXIIsA7v9zA7sAUQTg3w8aCyhK+PeDxgLo+BL+3aCygKKEfzewCxBGAP69oLOAooR/L7ALkEYA/p2gtICihH8nsAsQRwD+faC1gKKEfx+wC5BHAP5doLaAooR/F/ALkEcA/j2gt4BVCf8eqL0A3P+TKdwC4D93uAXg/r/c4RYA/7nDLQD+c4dbAPznDrcA+M8dbgHwnzvcAuA/d7gFwH/ucAuA/9z5087dtCYMBVEYnqsGS+0gQwm0TasJtJRaPxHn//80F24UJJOlnDnPOrv3hGRzb7QA9kcXLYD90UULYH900QLYH120APZHFy2A/dFFC2B/dNEC2B9dtAD2RxctgP3RRQtgf3TRAtgfXbCA7fWjC/YH1L+AsvmUi/GkY39Iat6nLPYv6/nk2PH8Dyo15/2Pqamxf25q7J+bGvvnpsb+uamxf25q7J+bGvvnpsb+uamxf25q7J+bGvvnpksfavovhKd68mGalRCkU/EBfmohUKvWI+W5EoJV/U6D138uBK2e9XwH2g8hePWx8buWo7FQBtVo9ua3yu7Ef79UDl+b77YpXt673d/rVoUeyBmd3r+p815HdQAAAABJRU5ErkJggg==
+'''
+Поиск самой нагруженной joint probe в первом Solution
+
+
+'''
+# Script:
+import ctypes
+Probes = Model.Analyses[0].Solution.GetChildren[Ansys.ACT.Automation.Mechanical.Results.ProbeResults.JointProbe](True)
+if Probes.Count == 0:
+    ctypes.windll.user32.MessageBoxW(0, "There are no joint probes defined!", "Error", 16)
+if not Model.Analyses[0].Solution.IsDataExtractionComplete:
+    ctypes.windll.user32.MessageBoxW(0, "There's no result file.\nSolve the case first!", "Error", 16)
+tMax = 0
+for i in Probes:
+    temp = i.PropertyByName("TotalVector").InternalValue
+    if (temp > tMax):
+        tMax = temp
+        maxProbe = i
+#a = solu.AddJointProbe()
+#a.BoundaryConditionSelection = maxProbe.BoundaryConditionSelection
+#a.Name = "Max from " + Folders[FolderNum].Name + " (" + maxProbe.Name + ")"
+maxProbe.Activate()
+x = maxProbe.PropertyByName("VectorXAxis").InternalValue
+y = maxProbe.PropertyByName("VectorYAxis").InternalValue
+Shear = (x**2+y**2)**0.5
+print('Most loaded joint')
+print('Reaction X = '+ maxProbe.PropertyByName("VectorXAxis").StringValue)
+print('Reaction Y = '+ maxProbe.PropertyByName("VectorYAxis").StringValue)
+print('Shear XY = '+ Shear.ToString() + " N")
+print('Reaction Z = '+ maxProbe.PropertyByName("VectorZAxis").StringValue)
+print('Total reaction = '+ maxProbe.PropertyByName("TotalVector").StringValue)
+text = 'Reaction X = '+ maxProbe.PropertyByName("VectorXAxis").StringValue + "\n" + \
+        'Reaction Y = '+ maxProbe.PropertyByName("VectorYAxis").StringValue + "\n" + \
+        'Shear XY = '+ Shear.ToString() + " N" + "\n" + \
+        'Reaction Z = '+ maxProbe.PropertyByName("VectorZAxis").StringValue + "\n" + \
+        'Total reaction = '+ maxProbe.PropertyByName("TotalVector").StringValue
+maxProbe.BoundaryConditionSelection.Activate()
+try:
+    joint = maxProbe.BoundaryConditionSelection
+    remPt = joint.PromoteToRemotePoint()[0]
+    nodeNum = remPt.GetMeshRegion().NodeIds[0]
+    print('APDL node num = ' + nodeNum.ToString())
+    text += "\n" + 'APDL node num = ' + nodeNum.ToString()
+except:
+    text += "\n\nAPDL node num is not available\nTo get it promote related joint probes to remote points manualy"
+ctypes.windll.user32.MessageBoxW(0, text, "Most loaded joint", 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
